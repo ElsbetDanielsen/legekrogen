732df2eaad230bad7a25eece5c681114dfc13df4
elsb0043 <157370211+elsb0043@users.noreply.github.com>
2025-02-07 10:50:10 +0100

    Færdig

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useRoutes, useLocation } from "react-router-dom"
+import { useRoutes, useLocation, Navigate } from "react-router-dom"
 
 import Fragt from "./components/fragt/Fragt.jsx"
 import NavBar from "./components/navigation/Navigation"
 import Kundeklub from "./components/kundeklub/Kundeklub.jsx"
 import Footer from "./components/footer/Footer"
-import Home from "./pages/Home"
 import Products from "./pages/Products.jsx"
 import Faq from "./pages/Faq.jsx"
 import Kundeklubben from "./pages/Kundeklubben.jsx"
 import SuccessPage from "./components/successPage/SuccessPage.jsx"
+import Home from "./pages/Home.jsx"
 
 function App() {
   const location = useLocation()
@@ -17,12 +17,13 @@ function App() {
   const isPage = ["/", "/products", "/faq"].includes(location.pathname)
 
 
-  const routes = useRoutes([
+  const routes = useRoutes([  
     { path: "/", element: <Home /> },
     { path: "/products", element: <Products /> },
     { path: "/faq", element: <Faq /> },
     { path: "/kundeklubben", element: <Kundeklubben /> },
     { path: "/success", element: <SuccessPage /> },
+    { path: "*", element: <Navigate to="/" /> },
   ])
 
   return (
